0deeb660c061fed003e66cf4ea4c7ac64ba4f553
Brian Kibet <kibetsbrian@gmail.com>
2023-10-01 13:54:22 +0300

    Add models

--- app/models.py
+++ app/models.py
@@ -6,5 +6,30 @@ class Hero(db.Model):
     __tablename__ = 'hero'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    
+class Power(db.Model):
+    __tablename__ = 'powers'
 
-# add any models you may need. 
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+    
+class HeroPower(db.Model):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.String, primary_key=True)
+    strength = db.Column(db.Integer)
+    hero_id = db.Column(db.String, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.String, db.ForeignKey('powers.id'))
+    created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    hero = db.relationship('Hero', backref='hero_powers')
+    power = db.relationship('Power', backref='hero_powers')
+
